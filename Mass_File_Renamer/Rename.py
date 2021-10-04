62df1fedc8de815763089e9a01a03111a4494ec0
PyNAABO <86821481+PyNAABO@users.noreply.github.com>
2021-10-04 12:20:46 +0530

    Update and rename mass_file_renamer.py to Rename.py

--- Mass_File_Renamer/Rename.py
+++ Mass_File_Renamer/Rename.py
@@ -1,8 +1,4 @@
-def rename(FROM,TO,reverse=False):
-    if reverse == True:
-        FROM,TO = TO,FROM
-    else: pass
-
+def rename(FROM,TO):
     import os
     a = os.listdir(os.getcwd())
     for i in a:
@@ -13,4 +9,4 @@ def rename(FROM,TO,reverse=False):
             os.rename(i,f"{i1}.{TO}")
             print(f"Renamed {i} to {i1}.{TO}")
 
-rename(input("Rename file extension from: "),input("Rename file extension To: "))
+rename(input("Rename file extension from: "),input("Rename To: "))
